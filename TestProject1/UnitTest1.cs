eb749bdf79faa5a7b5a33d3dd8fbafa2a8dd272b
Volodimir Chernysh <volodimirdomin@gmail.com>
2022-12-11 16:18:27 +0200

    TA02-57

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using SnackMachineLogic;
+
+namespace SnackMachineTest
+{
+    public class MoneyTest1
+    {
+        [Fact]
+        public void OperatorEqual_True_ReturnCorrectValue()
+        {
+            //Arrage
+            Money money1 = new Money(1, 0, 0, 4, 0, 0);
+            Money money2 = new Money(1, 0, 0, 4, 0, 0);
+            bool money = money1 == money2;
+            var expected = true;
+            //Asserd
+            Assert.Equal(expected, money);
+        }
+    }
+}
+
